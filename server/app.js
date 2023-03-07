b3111382e4297ea646b8bc625d0c94b242f53385
AngeloNicolson <angelonicolson@outlook.com>
2023-03-07 18:22:41 +1300

    Added route for forim post with the added api/ endpoint

--- server/app.js
+++ server/app.js
@@ -2,12 +2,14 @@ const express = require("express");
 const app = express();
 const cors = require("cors");
 const pool = require("./db");
+const createForumRouter = require("./forum/createForum.router");
 
 // Middleware
 app.use(cors());
 app.use(express.json()); //req.body
 
 // ROUTES
+app.use("/api/new-forum", createForumRouter);
 
 app.get("/", async (request, response) => {
   try {
