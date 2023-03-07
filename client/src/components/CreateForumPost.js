8310283862fc62d3030a91dbec61fc8308dfea4c
AngeloNicolson <angelonicolson@outlook.com>
2023-03-08 08:55:58 +1300

    Fixed connection between client and server. Had a successful post from the clinet form

--- client/src/components/CreateForumPost.js
+++ client/src/components/CreateForumPost.js
@@ -2,17 +2,18 @@ import React, { useState } from "react";
 
 const CreatePost = () => {
   const [text, setText] = useState("");
+  const [topic, SetTopic] = useState("");
+  const [userID, SetUserID] = useState("");
 
   const handleSubmit = async () => {
     try {
       const body = {
         post_text: text,
-        updated_at: updated,
         topic: topic,
-        author: userId,
+        author: userID,
       };
 
-      await fetch(`localhost:5000/api/new-forum`, {
+      await fetch(`${process.env.REACT_APP_API_URL}/api/new-forum`, {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify(body),
@@ -26,16 +27,35 @@ const CreatePost = () => {
     <>
       <div>
         <form onSubmit={handleSubmit}>
-          <h1>Start forum</h1>
+          <h1>Start a forum</h1>
           <label>Text</label>
           <input
             type="text"
-            value={Text}
+            value={text}
             onChange={(event) => {
               setText(event.target.value);
             }}
             required
           />
+          <label>Topic</label>
+          <input
+            type="text"
+            value={topic}
+            onChange={(event) => {
+              SetTopic(event.target.value);
+            }}
+            required
+          />
+          <label>userID</label>
+          <input
+            type="text"
+            value={userID}
+            onChange={(event) => {
+              SetUserID(event.target.value);
+            }}
+            required
+          />
+
           <button type="submit" value="submit">
             Post forum
           </button>
