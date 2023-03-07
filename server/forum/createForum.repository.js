9f6c2f82187baac893dc621d7a848afd166486ff
AngeloNicolson <angelonicolson@outlook.com>
2023-03-07 21:33:52 +1300

    Fixed variable and syntax mistakes in the repository query. All posting to database correctly

--- server/forum/createForum.repository.js
+++ server/forum/createForum.repository.js
@@ -8,9 +8,9 @@ const createPost = async (post_text, updated_at, topic, author) => {
     const Pool = await pool();
 
     const newPost = await Pool.query(
-      `INSERT INTO post (post_text, created_at, updated_at, topic, author) VALUES($1, $2, $3, $4, $5) 
-      RETURNING post_id post_text, created_at, updated_at, topic, author`,
-      [post_text, created_at, updated_at, topic, author]
+      `INSERT INTO post (post_text, updated_at, topic, author, created_at) VALUES($1, $2, $3, $4, $5) 
+      RETURNING post_id, post_text, created_at, updated_at, topic, author`,
+      [post_text, updated_at, topic, author, created_at]
     );
 
     return newPost.rows[0];
