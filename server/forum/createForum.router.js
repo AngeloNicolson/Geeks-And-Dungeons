7c67796be6799c3e104246e39c08b35feaf6b7f8
AngeloNicolson <angelonicolson@outlook.com>
2023-03-07 19:13:14 +1300

    Updating SQL query

--- server/forum/createForum.router.js
+++ server/forum/createForum.router.js
@@ -1,12 +1,19 @@
-app.post("/", async (request, response) => {
-  try {
-    const { title } = request.body;
-    const newTopic = await pool.query("INSERT INTO topic (title) VALUES($1)", [
-      title,
-    ]);
+const express = require("express");
+const router = express.Router();
 
-    response.json(newTopic);
+router.post("/", async (request, response) => {
+  try {
+    const { post_text, created_at, updated_at, topic, author } = request.body;
+    const newPost = await pool.query(
+      `INSERT INTO post (post_text, created_at, updated_at, topic, author) VALUES($1 $2 $3 $4) 
+      RETURNING post_id post_text, created_at, updated_at, topic, author`,
+      [post_text, created_at, updated_at, topic, author]
+    );
+
+    response.json(newPost);
   } catch (err) {
     console.error(err.message);
   }
 });
+
+modules.export = router;
