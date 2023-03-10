e73625b53a44c36feb5b83e6673c1e57effe96d6
AngeloNicolson <angelonicolson@outlook.com>
2023-03-11 11:17:35 +1300

    Changed scrolling control to the segment. This makes more sense since the segment should control the components inside

--- client/src/components/Segment/Segment.js
+++ client/src/components/Segment/Segment.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useRef } from "react";
 
 import styles from "./Segment.module.css";
 import TopicCardList from "../Topics/TopicCardList";
@@ -8,11 +8,55 @@ import topic_data from "../../MockData/topicMockData";
 
 // The segment is in control of the title and taking in the card categories
 const Segment = ({ title }) => {
+  let scrl = useRef(null);
   const [topiccards, setTopicCard] = useState(topic_data);
+  const [scrollX, setscrollX] = useState(0);
+  const [scrolEnd, setscrolEnd] = useState(false);
+
+  //Slide click
+  const slide = (shift) => {
+    scrl.current.scrollLeft += shift;
+    setscrollX(scrollX + shift);
+
+    if (
+      Math.floor(scrl.current.scrollWidth - scrl.current.scrollLeft) <=
+      scrl.current.offsetWidth
+    ) {
+      setscrolEnd(true);
+    } else {
+      setscrolEnd(false);
+    }
+  };
+
+  const scrollCheck = () => {
+    setscrollX(scrl.current.scrollLeft);
+    if (
+      Math.floor(scrl.current.scrollWidth - scrl.current.scrollLeft) <=
+      scrl.current.offsetWidth
+    ) {
+      setscrolEnd(true);
+    } else {
+      setscrolEnd(false);
+    }
+  };
   return (
     <div>
+      //Left
+      {scrollX !== 0 && (
+        <button className="prev" onClick={() => slide(-500)}>
+          <i className="fa fa-angle-left"></i>
+        </button>
+      )}
       <p className={styles.title}>{title}</p>
-      <TopicCardList topiccards={topiccards} />
+      <div className={styles.segment} ref={scrl} onScroll={scrollCheck}>
+        <TopicCardList topiccards={topiccards} />
+      </div>
+      //right
+      {!scrolEnd && (
+        <button className="next" onClick={() => slide(+500)}>
+          <i className="fa fa-angle-right"></i>
+        </button>
+      )}
     </div>
   );
 };
--- client/src/components/Topics/TopicCardList.js
+++ client/src/components/Topics/TopicCardList.js
@@ -2,55 +2,13 @@ import React, { useState, useRef } from "react";
 import TopicCard from "./TopicCard";
 
 export default function TopicCardList({ topiccards }) {
-  let scrl = useRef(null);
-  const [scrollX, setscrollX] = useState(0);
-  const [scrolEnd, setscrolEnd] = useState(false);
-
-  //Slide click
-  const slide = (shift) => {
-    scrl.current.scrollLeft += shift;
-    setscrollX(scrollX + shift);
-
-    if (
-      Math.floor(scrl.current.scrollWidth - scrl.current.scrollLeft) <=
-      scrl.current.offsetWidth
-    ) {
-      setscrolEnd(true);
-    } else {
-      setscrolEnd(false);
-    }
-  };
-
-  const scrollCheck = () => {
-    setscrollX(scrl.current.scrollLeft);
-    if (
-      Math.floor(scrl.current.scrollWidth - scrl.current.scrollLeft) <=
-      scrl.current.offsetWidth
-    ) {
-      setscrolEnd(true);
-    } else {
-      setscrolEnd(false);
-    }
-  };
   return (
     <div>
-      //Left
-      {scrollX !== 0 && (
-        <button className="prev" onClick={() => slide(-500)}>
-          <i className="fa fa-angle-left"></i>
-        </button>
-      )}
-      <div className="card-grid" ref={scrl} onScroll={scrollCheck}>
+      <div className="card-grid">
         {topiccards.map((topicCard) => {
           return <TopicCard topicCard={topicCard} key={topicCard.topic_id} />;
         })}
       </div>
-      //right
-      {!scrolEnd && (
-        <button className="next" onClick={() => slide(+500)}>
-          <i className="fa fa-angle-right"></i>
-        </button>
-      )}
     </div>
   );
 }
