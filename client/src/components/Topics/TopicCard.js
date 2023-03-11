3eded96592a39545482ab36cc2014d526cd76b6e
AngeloNicolson <angelonicolson@outlook.com>
2023-03-12 09:43:04 +1300

    Storing the choice one and two of the cards

--- client/src/components/Topics/TopicCard.js
+++ client/src/components/Topics/TopicCard.js
@@ -1,15 +1,30 @@
 import React, { useState } from "react";
 
-export default function TopicCard({ topicCard }) {
+export default function TopicCard({ topicCard, handleChoice }) {
   const [flip, setFlip] = useState(false);
+  // const [flipped, setFlipped] = useState(false);
+  //
+  // Need to finda  way to deselc other cards so they flip back over
+  //
+  // const flipCard = () => {
+  //   if (flip === true) {
+  //     setFlipped(true);
+  //   } else {
+  //     setFlipped(true);
+  //   }
+  // };
+
+  const handleClick = () => {
+    handleChoice(topicCard);
+  };
 
   return (
     <div
       className={`card ${flip ? "flip" : ""}`}
       onClick={() => setFlip(!flip)}
     >
-      <div className="front">{topicCard.title}</div>
-      <div className="back">
+      <div className="back">{topicCard.title}</div>
+      <div className="front" onClick={handleClick}>
         {topicCard.title} " "{topicCard.category}
       </div>
     </div>
--- client/src/components/Topics/TopicCardList.js
+++ client/src/components/Topics/TopicCardList.js
@@ -1,11 +1,27 @@
+import { useState } from "react";
 import TopicCard from "./TopicCard";
 
 export default function TopicCardList({ topiccards }) {
+  // This will store the state ov which card has been selected.
+  // This way we can the de-select it and flip it back over when onther is chosen.
+  const [cardChoiceOne, setCarChoiceOne] = useState;
+  const [cardChoiceTwo, setCarChoiceTwo] = useState;
+
+  const handleChoice = (topicCard) => {
+    cardChoiceOne ? setCarChoiceTwo(topicCard) : setCarChoiceOne(topicCard);
+  };
+
   return (
     <div>
       <div className="card-grid">
         {topiccards.map((topicCard) => {
-          return <TopicCard topicCard={topicCard} key={topicCard.topic_id} />;
+          return (
+            <TopicCard
+              topicCard={topicCard}
+              key={topicCard.topic_id}
+              handleChoice={handleChoice}
+            />
+          );
         })}
       </div>
     </div>
