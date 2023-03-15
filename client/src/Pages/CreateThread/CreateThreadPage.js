c41393c608112d0a3cbe20baf2ea3415ecc26895
AngeloNicolson <angelonicolson@outlook.com>
2023-03-16 11:07:02 +1300

    Added quill to the createThreadPage. Basic styling has been included. with the way that quill is set up, it is hard to add css modules for styling so have keept the normal styling in this instance

--- client/src/Pages/CreateThread/CreateThreadPage.js
+++ client/src/Pages/CreateThread/CreateThreadPage.js
@@ -2,18 +2,25 @@
 import { React, useState } from "react";
 import Navigation from "../../components/Navigation/Navigation.js";
 import Segment from "../../components/Segment/Segment";
+import ReactQuill from "react-quill";
+import "react-quill/dist/quill.snow.css";
 
 // STYLES
 import "./CreateThreadPage.css";
 import styles from "../PageLayout.module.css";
+import QuillToolbar, {
+  modules,
+  formats,
+} from "../../components/TextEditor/TextEditor";
 
 // API
 import api from "../../Api";
 
 function CreateThreadPage() {
-  const [text, setText] = useState("");
+  const [text, SetText] = useState("");
   const [topic, SetTopic] = useState(0);
   const [userID, SetUserID] = useState("");
+  // const [value, setValue] = useState("");
 
   const handleSubmit = async () => {
     try {
@@ -23,45 +30,68 @@ function CreateThreadPage() {
     }
   };
   // This function pulls the id from the segment child.
-  // This is a work around for passing prop to parent. (May need to change in future depending on react)
-  function getCardIdFromSegment(topic_id) {
+  // This is a work around for passing prop to parent.
+  // (May need to change in future depending on how react handles this functionality in the future. Or if I use a library to handle this)
+  function getCardId(topic_id) {
     SetTopic(topic_id);
   }
-  console.log(topic);
   return (
     <>
       <Navigation />
       <div className={styles.body_inner}>
         <h1>Create thread</h1>
-        <Segment title="games" getCardIdFromSegment={getCardIdFromSegment} />
-        <div>
-          <form onSubmit={handleSubmit}>
-            <label>Text</label>
-            <textarea
-              name="postContent"
-              value={text}
-              onChange={(event) => setText(event.target.value)}
-              required
-            />
-            <br></br>
-            <br></br>
-            <label>userID</label>
-            <input
-              type="text"
-              value={userID}
-              onChange={(event) => {
-                SetUserID(event.target.value);
-              }}
-              required
-            />
+        <textarea
+          className={styles.threadTitleEntry}
+          type="text"
+          rows={1}
+          placeholder={"Thread Title"}
+          value={userID}
+          onChange={(event) => {
+            SetUserID(event.target.value);
+          }}
+        />
 
-            <button type="submit" value="submit">
+        <Segment title="games" getCardId={getCardId} />
+        <form onSubmit={handleSubmit}>
+          <label>Text</label>
+          <label>userID</label>
+          <input
+            type="text"
+            value={userID}
+            onChange={(event) => {
+              SetUserID(event.target.value);
+            }}
+            required
+          />
+          <br />
+          <div className={styles.editorContainer}>
+            <QuillToolbar toolbarId={"t1"} />
+
+            <ReactQuill
+              theme="snow"
+              value={text}
+              onChange={SetText}
+              placeholder={"Start your awesome thread..."}
+              modules={modules("t1")}
+              formats={formats}
+            />
+            <button
+              type="submit"
+              value="submit"
+              className={styles.threadButton}
+            >
               Post forum
             </button>
-          </form>
-        </div>
+          </div>
+        </form>
       </div>
     </>
   );
 }
 export default CreateThreadPage;
+
+/*
+-------------------------------------
+            CREDITS
+-------------------------------------
+ */
