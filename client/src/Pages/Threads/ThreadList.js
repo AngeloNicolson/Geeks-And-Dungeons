92e3f09901bb20a443c8b1e8992c4434f77f1f50
AngeloNicolson <angelonicolson@outlook.com>
2023-04-16 13:14:51 +1200

    Changed layout of thread card so that it is easier to understand

--- client/src/Pages/Threads/ThreadList.js
+++ client/src/Pages/Threads/ThreadList.js
@@ -8,16 +8,19 @@ const ThreadFeed = ({ threads }) => {
       <ul className={styles.threadCard}>
         {threads.map((thread) => (
           <div key={thread.thread_id} className={styles.card_layout}>
+            <div className={styles.button_container}>
+              <div className={styles.thread_button}>
+                <EyeButton onMouseMove={(event) => this.handleMouse(event)} />
+              </div>
+            </div>
+            <p className={styles.button_title}>
+              A mysterious being becons you to enter
+            </p>
             <li className={styles.threadGrid}>
               <h3 className={styles.threadTitle}>{thread.thread_title}</h3>
               <p>{thread.thread_text}</p>
               <p>{formatDate(thread.created_at)}</p>
             </li>
-            <div>
-              <div className={styles.thread_button}>
-                <EyeButton onMouseMove={(event) => this.handleMouse(event)} />
-              </div>
-            </div>
           </div>
         ))}
       </ul>
