92dc55ff68efac36f9a9eb0269964def34b3ef46
AngeloNicolson <angelonicolson@outlook.com>
2023-05-05 19:37:21 +1200

    Added jwt to check authorization in API

--- server/app.js
+++ server/app.js
@@ -1,15 +1,25 @@
+// MIDDLEWARE
 const express = require("express");
 const app = express();
 const cors = require("cors");
+const { auth } = require("express-oauth2-jwt-bearer");
+
+// ROUTES
 const postThreadRouter = require("./forum/postThread.router");
 const getThreadRouter = require("./forum/getThreads.router");
 
+const jwtCheck = auth({
+  audience: "https://gnd-api",
+  issuerBaseURL: "https://angelo-nicolson.au.auth0.com/",
+  tokenSigningAlg: "RS256",
+});
+
 // Middleware
 app.use(cors());
+app.use(jwtCheck);
 app.use(express.json()); //req.body
-// Function to serve all static files
-// inside public directory.
-app.use(express.static("public"));
+
+app.use(express.static("public")); // Function to serve all static files inside public directory.
 app.use("/images", express.static("images"));
 
 // ROUTES
