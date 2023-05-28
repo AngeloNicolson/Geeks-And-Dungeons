ef2336dd5855d7e7f4b81234a5c6d97112ace454
AngeloNicolson <angelonicolson@outlook.com>
2023-05-29 09:00:02 +1200

    Pushing changes so i can pull down without wsl

--- server/db.js
+++ server/db.js
@@ -1,12 +1,22 @@
 const Pool = require("pg").Pool;
-
 let pool = null;
 
+const user = process.env.PGDATABASE;
+const password = process.env.PGPASSWORD;
 async function get_pool() {
   if (pool !== null) {
     return pool;
   }
-  pool = new Pool({});
+  pool = new Pool({
+    /*
+    -------------------------------------------------------
+    // USER and PASSWORD NOT WORKING FROM .env file
+    // need a way to get these variables from .env to here
+    -------------------------------------------------------
+   */
+    user: "postgres",
+    password: "password",
+  });
 
   await pool.connect();
 
--- server/forum/thread.test.js
+++ server/forum/thread.test.js
@@ -1,3 +1,29 @@
-import request from "supertest";
-import express from "express";
-import app from "../app";
+const request = require("supertest");
+const express = require("express");
+const app = require("../app");
+const get_pool = require("../db");
+
+// MOCK DATA
+const topic_data = require("../MockData/topicMockData");
+
+const repository = require("./getThreads.repository");
+
+describe("GIVEN that the /api/get-threads route exists", () => {
+  // This will shutdown the pool call after every test to ensure there are no memory leaks with open pool querries
+  // DOC'S https://node-postgres.com/features/pooling
+  afterAll(async () => {
+    const database = await get_pool();
+    database.end();
+  });
+
+  test("WHEN there are threads in the database then return status 200 and an array of threads", async () => {
+    const getThreads = await repository.getThreads();
+    const expectedResStatus = 200;
+    const mockedResponsData = topic_data;
+    const response = await request(app)
+      .get("/api/get-threads")
+      .set("Accept", "application/json");
+    expect(response.status).toBe(expectedResStatus);
+    expect(getThreads).toEqual(mockedResponsData);
+  });
+});
