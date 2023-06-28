3bd73b9bc7d8535cbd596eddae40524a70a40d0c
AngeloNicolson <69103703+AngeloNicolson@users.noreply.github.com>
2023-06-28 20:07:59 +1200

    Updating the thread list so that it is mor profesional. as the buttons were to much. So they have been removed and will be used elswhere

--- client/src/Pages/Threads/ThreadList.js
+++ client/src/Pages/Threads/ThreadList.js
@@ -1,32 +1,21 @@
 import styles from "./ThreadList.module.css";
 import { formatDate } from "../../Utils/formatDate";
 
-import EyeButton from "../../components/Buttons/EyeButton/EyeButton";
 const ThreadFeed = ({ threads }) => {
   return (
-    <>
-      <ul className={styles.threadCard}>
-        {threads.map((thread) => (
-          <div key={thread.thread_id} className={styles.card_layout}>
-            <div className={styles.button_container}>
-              <div className={styles.thread_button}>
-                <EyeButton onMouseMove={(event) => this.handleMouse(event)} />
-              </div>
-            </div>
-            <p className={styles.button_title}>
-              A mysterious being becons you to enter
-            </p>
-            <li className={styles.threadGrid}>
-              <h3 className={styles.threadTitle}>{thread.thread_title}</h3>
-              <p>{thread.thread_text}</p>
-              <p>{formatDate(thread.created_at)}</p>
-            </li>
+    <div className={styles.threadCard}>
+      {threads.map((thread) => (
+        <div key={thread.thread_id} className={styles.threadItem}>
+          <div className={styles.threadContent}>
+            <h3 className={styles.threadTitle}>{thread.thread_title}</h3>
+            <p className={styles.date}>{formatDate(thread.created_at)}</p>
           </div>
-        ))}
-      </ul>
-    </>
+        </div>
+      ))}
+    </div>
   );
 };
+
 export default ThreadFeed;
 
 /*
@@ -34,5 +23,3 @@ export default ThreadFeed;
         CREDITS
 ---------------------------
 */
-// https://codepen.io/Fieve/pen/bogzQW
-// https://developer.mozilla.org/en-US/docs/Web/SVG/Element/feGaussianBlur
