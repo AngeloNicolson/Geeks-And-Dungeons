9d6a88479d4b7cdf1e61466a97a826137bb123e8
AngeloNicolson <69103703+AngeloNicolson@users.noreply.github.com>
2023-06-30 19:25:58 +1200

    Adding the create thread button to the navigation and combining the secondary navigation into the main one

--- client/src/Pages/Threads/ThreadsPage.js
+++ client/src/Pages/Threads/ThreadsPage.js
@@ -1,9 +1,7 @@
 // PAGE ELEMENTS
 import { React, useState, useEffect } from "react";
-import { useNavigate } from "react-router-dom";
 import Navigation from "../../components/Navigation/Navigation.js";
 import ThreadFeed from "./ThreadList.js";
-import CreatureButton from "../../components/Buttons/CreatureButton/CreatureButton.js";
 
 // STYLES
 import styles from "../PageLayout.module.css";
@@ -12,7 +10,6 @@ import api from "../../Api";
 
 function ThreadPage() {
   const [threads, setThreads] = useState([]);
-  const navigate = useNavigate();
   useEffect(() => {
     const fetchData = async () => {
       try {
@@ -26,19 +23,10 @@ function ThreadPage() {
     fetchData();
   }, []);
 
-  const navigateToCreateThread = () => {
-    navigate("/createthread");
-  };
   return (
     <>
       <Navigation />
       <div className={styles.body_inner}>
-        <div className={styles.createThreadButton}>
-          <CreatureButton onClick={navigateToCreateThread}>
-            Creatfe thread
-          </CreatureButton>
-        </div>
-
         <div className={styles.div_identification}>
           <ThreadFeed threads={threads} />
         </div>
--- client/src/components/Navigation/Navigation.js
+++ client/src/components/Navigation/Navigation.js
@@ -1,30 +1,60 @@
 import React from "react";
-
-// Element imports
-import SecondaryNavigation from "./SecondaryNavigation";
-
-// CSS modules
 import styles from "./Navigation.module.css";
+import { useAuth0 } from "@auth0/auth0-react";
+import { LogoutButton } from "../Buttons/Login-Logout/logout-button";
+import { LoginButton } from "../Buttons/Login-Logout/login-button";
+import CreatureButton from "../Buttons/CreatureButton/CreatureButton";
 
 const Navigation = () => {
+  const { isAuthenticated } = useAuth0();
+
   return (
     <div className={styles.mainNavigation}>
-      <ul className={styles.siteNavigation}>
-        <li>
-          <a href="/" className={styles.navigationItem}>
-            FORUMS
-          </a>
-        </li>
-        <li>
-          <a href="meetpeople" className={styles.navigationItem}>
-            MEET PEOPLE
-          </a>
-        </li>
-      </ul>
-      <a href="/home" className={styles.logo_home}>
-        LOGO HERE
-      </a>
-      <SecondaryNavigation />
+      <div className={styles.leftNavigation}>
+        <ul className={styles.siteNavigation}>
+          <li>
+            <a href="/" className={styles.navigationItem}>
+              FORUMS
+            </a>
+          </li>
+          <li>
+            <a href="meetpeople" className={styles.navigationItem}>
+              MEET PEOPLE
+            </a>
+          </li>
+        </ul>
+      </div>
+      <div className={styles.centerNavigation}>
+        <CreatureButton />
+      </div>
+      <div className={styles.rightNavigation}>
+        <ul className={styles.personalNavigation}>
+          {!isAuthenticated && (
+            <>
+              <li>
+                <a href="/profile" className={styles.navigationItem}>
+                  PROFILE
+                </a>
+              </li>
+              <li className={styles.navigationItem}>
+                <LoginButton />
+              </li>
+            </>
+          )}
+          {isAuthenticated && (
+            <>
+              <li>
+                <a href="/profile" className={styles.navigationItem}>
+                  PROFILE
+                </a>
+              </li>
+              <li className={styles.navigationItem}>
+                <LogoutButton />
+              </li>
+            </>
+          )}
+        </ul>
+      </div>
     </div>
   );
 };
