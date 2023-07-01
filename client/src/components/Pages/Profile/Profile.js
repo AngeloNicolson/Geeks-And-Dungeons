fe89f2306b687b0a8fcc4134c9ca31cf17cf6334
AngeloNicolson <69103703+AngeloNicolson@users.noreply.github.com>
2023-07-02 11:53:57 +1200

    Storing the username on login. this way avoids forcing the user to make a username for the time being

--- client/src/components/Pages/Profile/Profile.js
+++ client/src/components/Pages/Profile/Profile.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { useAuth0 } from "@auth0/auth0-react";
 import api from "../../../Api";
 import styles from "./Profile.module.css";
@@ -7,45 +7,34 @@ import Navigation from "../../Navigation/Navigation";
 const Profile = () => {
   const { user, isAuthenticated, isLoading } = useAuth0();
   const [username, setUsername] = useState("");
-  const [isUsernameSet, setIsUsernameSet] = useState(false);
 
   useEffect(() => {
-    // Check username on load
-    const checkUsername = async () => {
+    const storeUsername = async () => {
       try {
-        const userProfile = await api.getUserProfile(user.sub);
-        if (userProfile !== undefined && userProfile[0].username) {
-          setUsername(userProfile[0].username);
-          setIsUsernameSet(true);
+        if (isAuthenticated && user && user.sub && user.nickname) {
+          const userProfile = await api.getUserProfile(user.sub);
+          if (
+            !(userProfile && userProfile.length > 0 && userProfile[0].username)
+          ) {
+            await api.updateUserProfile(user.sub, user.nickname);
+            console.log("username set");
+          } else {
+            setUsername(userProfile[0].username);
+          }
         }
-        console.log(userProfile[0].username);
       } catch (error) {
-        console.error("Error retrieving user profile:", error);
+        console.error("Error storing username:", error);
       }
     };
 
-    if (isAuthenticated) {
-      checkUsername();
-    }
-  }, [isAuthenticated, user.sub]);
-
-  const handleUsernameSubmit = async (e) => {
-    e.preventDefault();
-
-    try {
-      await api.updateUserProfile(user.sub, username);
-      setIsUsernameSet(true);
-    } catch (error) {
-      console.error("Error updating user profile:", error);
-    }
-  };
+    storeUsername();
+  }, [isAuthenticated, user]);
 
   if (isLoading) {
     return <div>Loading...</div>;
   }
 
   if (!isAuthenticated) {
-    // Handle the case where the user is not authenticated
     return (
       <div className={styles.profileContainer}>
         <Navigation />
@@ -54,40 +43,15 @@ const Profile = () => {
     );
   }
 
-  if (isUsernameSet) {
-    // Handle the case where the username is already set
-    return (
-      <div className={styles.profileContainer}>
-        <Navigation />
-        <img src={user.picture} alt="Profile" />
-        <h2 className={styles.profileText}>{user.name}</h2>
-        <p className={styles.profileText}>{user.email}</p>
-        <p className={styles.profileText}>Username: {username}</p>
-      </div>
-    );
-  }
+  const displayName = username || user.nickname;
 
-  // Render the form to set the username
   return (
     <div className={styles.profileContainer}>
       <Navigation />
       <img src={user.picture} alt="Profile" />
-      <h2 className={styles.profileText}>{user.name}</h2>
+      <h2 className={styles.profileText}>{displayName}</h2>
       <p className={styles.profileText}>{user.email}</p>
-      <form onSubmit={handleUsernameSubmit}>
-        <label>
-          Username:
-          <input
-            type="text"
-            value={username}
-            onChange={(e) => setUsername(e.target.value)}
-            className={styles.input}
-          />
-        </label>
-        <button type="submit" className={styles.button}>
-          Set Username
-        </button>
-      </form>
+      <p className={styles.profileText}>{displayName}</p>
     </div>
   );
 };
