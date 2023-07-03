44110988caceb8a55c90d629eaa2e957725ce76d
AngeloNicolson <69103703+AngeloNicolson@users.noreply.github.com>
2023-07-03 20:12:47 +1200

    Standardizing file format and adding the query validation and sanitization

--- server/forum/getThreads.router.js
+++ server/forum/getThreads.router.js
@@ -1,17 +1,44 @@
+// MIDDLEWARE IMPORTS
 const express = require("express");
 const router = express.Router();
+const Joi = require("joi");
+const queryValidationMiddleware = require("../middleware/queryValidationMiddleware");
+
+// UTILS IMPORTS
+const sanitizeInput = require("../utils/sanitization");
+
+// REPOSITORY IMPORTS
 const repository = require("./getThreads.repository");
 
-router.get("/:id", async (request, response, next) => {
-  try {
-    const { id } = request.params;
-    const forumPost = await repository.getSingleThread(id);
-    return response.json(forumPost);
-  } catch (error) {
-    console.error(error.message);
-    next(error);
-  }
+/*
+----------------------------------
+     JOI VALIDATION SCHEMA
+----------------------------------
+*/
+const getSingleThreadSchema = Joi.object({
+  id: Joi.number().integer().positive(),
 });
+
+/*
+----------------------------------
+           ROUTERS
+----------------------------------
+*/
+
+router.get(
+  "/:id",
+  queryValidationMiddleware(getSingleThreadSchema),
+  async (request, response, next) => {
+    try {
+      const { id } = request.params;
+      const forumPost = await repository.getSingleThread(sanitizeInput(id));
+      return response.json(forumPost);
+    } catch (error) {
+      console.error(error.message);
+      next(error);
+    }
+  }
+);
 
 router.get("/", async (request, response, next) => {
   try {
