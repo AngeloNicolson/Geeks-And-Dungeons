66d80d3c9cde99f67d1a75b7a871686dba6bf95e
AngeloNicolson <69103703+AngeloNicolson@users.noreply.github.com>
2023-07-03 19:13:54 +1200

    Adding getsingle thread router and repository function for getting a single thread

--- server/forum/getThreads.repository.js
+++ server/forum/getThreads.repository.js
@@ -5,21 +5,33 @@ const pool = require("../db");
            SQL QUERRIES
 -----------------------------------
 */
+const getSinglePostSQL = `SELECT id, title, content, created_at FROM thread WHERE id = $1`;
 
-const getPostSQL = `SELECT * FROM thread`;
+const getPostsSQL = `SELECT * FROM thread`;
 /* 
 -----------------------------------
        REPOSITORY FUNCTIONS
 -----------------------------------
 */
-const getThreads = async () => {
+const getSingleThread = async () => {
   try {
     const Pool = await pool();
-
-    const result = await Pool.query(getPostSQL);
+    const values = [id];
+    const result = await Pool.query(getSinglePostSQL, values);
     return result.rows;
   } catch (error) {
     throw new Error("Error getting threads");
   }
 };
-module.exports = { getThreads };
+
+const getThreads = async () => {
+  try {
+    const Pool = await pool();
+
+    const result = await Pool.query(getPostsSQL);
+    return result.rows;
+  } catch (error) {
+    throw new Error("Error getting threads");
+  }
+};
+module.exports = { getThreads, getSingleThread };
--- server/forum/getThreads.router.js
+++ server/forum/getThreads.router.js
@@ -2,6 +2,16 @@ const express = require("express");
 const router = express.Router();
 const repository = require("./getThreads.repository");
 
+router.get("/:id", async (request, response, next) => {
+  try {
+    const forumPosts = await repository.getSingleThread();
+    return response.json(forumPosts);
+  } catch (error) {
+    console.error(error.message);
+    next(error);
+  }
+});
+
 router.get("/", async (request, response, next) => {
   try {
     const forumPosts = await repository.getThreads();
