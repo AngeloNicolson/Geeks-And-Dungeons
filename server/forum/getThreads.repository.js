90ef268713cbc9d3e5e49e6b11718649edd67c48
AngeloNicolson <69103703+AngeloNicolson@users.noreply.github.com>
2023-07-03 19:55:13 +1200

    Updating the sql qurry to pull the correct columns from the table

--- server/forum/getThreads.repository.js
+++ server/forum/getThreads.repository.js
@@ -5,7 +5,7 @@ const pool = require("../db");
            SQL QUERRIES
 -----------------------------------
 */
-const getSinglePostSQL = `SELECT id, title, content, created_at FROM thread WHERE id = $1`;
+const getSinglePostSQL = `SELECT thread_id, thread_title, thread_text, created_at, topic_id, author FROM thread WHERE thread_id = $1`;
 
 const getPostsSQL = `SELECT * FROM thread`;
 /* 
@@ -13,14 +13,15 @@ const getPostsSQL = `SELECT * FROM thread`;
        REPOSITORY FUNCTIONS
 -----------------------------------
 */
-const getSingleThread = async () => {
+const getSingleThread = async (thread_id) => {
   try {
     const Pool = await pool();
-    const values = [id];
+
+    const values = [thread_id];
     const result = await Pool.query(getSinglePostSQL, values);
     return result.rows;
   } catch (error) {
-    throw new Error("Error getting threads");
+    throw new Error("Error getting thread");
   }
 };
 
