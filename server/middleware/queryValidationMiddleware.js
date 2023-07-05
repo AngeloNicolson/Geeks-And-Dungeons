144d28834c4200c892b4c0e30241c380a8ceb953
AngeloNicolson <69103703+AngeloNicolson@users.noreply.github.com>
2023-07-05 19:13:18 +1200

    Adding validation for parameters specifically for handling the reply route and url which takes in a input

--- server/middleware/queryValidationMiddleware.js
+++ server/middleware/queryValidationMiddleware.js
@@ -1,20 +1,40 @@
-const queryValidationMiddleware = (schema) => (request, response, next) => {
-  try {
-    const { error } = schema.validate(request.body, { abortEarly: false });
-    if (error) {
-      const errorMessages = error.details.map((detail) => {
-        return {
-          field: detail.context.label,
-          message: detail.message.replace(/['"]/g, ""),
-        };
-      });
-      return response.status(400).json({ errors: errorMessages });
-    } else {
-      next();
-    }
-  } catch (error) {
-    next(error);
+const validateSchema = (schema, data) => {
+  if (!schema) return null;
+
+  const { error } = schema.validate(data);
+  if (error) {
+    const errorMessages = error.details.map((detail) => ({
+      field: detail.context.key,
+      message: detail.message.replace(/['"]/g, ""),
+    }));
+    return errorMessages;
   }
+
+  return null;
 };
+
+const queryValidationMiddleware =
+  (paramSchema, bodySchema) => (request, response, next) => {
+    try {
+      const paramErrors = validateSchema(paramSchema, request.params);
+      const bodyErrors = validateSchema(bodySchema, request.body);
+
+      const errors = [];
+      if (paramErrors) {
+        errors.push(...paramErrors);
+      }
+      if (bodyErrors) {
+        errors.push(...bodyErrors);
+      }
+
+      if (errors.length > 0) {
+        return response.status(400).json({ errors });
+      } else {
+        next();
+      }
+    } catch (error) {
+      next(error);
+    }
+  };
 
 module.exports = queryValidationMiddleware;
