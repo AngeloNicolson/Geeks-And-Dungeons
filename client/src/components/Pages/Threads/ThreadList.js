32290aa6ca42b2e0973f853559e6dec5fb07c2a8
AngeloNicolson <69103703+AngeloNicolson@users.noreply.github.com>
2023-07-08 12:12:28 +1200

    Adding button bar with basic layout and styles

--- client/src/components/Pages/Threads/ThreadList.js
+++ client/src/components/Pages/Threads/ThreadList.js
@@ -1,30 +1,77 @@
+import React, { useState } from "react";
 import styles from "./ThreadList.module.css";
-import { Link } from "react-router-dom";
 import { formatDate } from "../../../Utils/formatDate";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faEllipsisH } from "@fortawesome/free-solid-svg-icons";
 
 const ThreadFeed = ({ threads }) => {
+  const [showDeleteDropdown, setShowDeleteDropdown] = useState(false);
+
+  const handleDeleteThread = (threadId) => {
+    console.log("Delete thread with ID:", threadId);
+  };
+
+  const toggleDeleteDropdown = (event) => {
+    event.stopPropagation();
+    setShowDeleteDropdown((prevState) => !prevState);
+  };
+
+  const handleButtonClick = (event) => {
+    event.stopPropagation();
+    console.log("button clicked:");
+  };
+
   return (
     <div className={styles.threadCard}>
       {threads.map((thread) => (
-        <Link
-          to={`/thread/${thread.thread_id}`}
+        <div
+          className={styles.threadItem}
           key={thread.thread_id}
-          style={{ textDecoration: "none", color: "inherit" }}
+          onClick={() => {
+            window.location.href = `/thread/${thread.thread_id}`;
+          }}
         >
-          <div key={thread.thread_id} className={styles.threadItem}>
-            <div className={styles.threadContent}>
-              <h3 className={styles.threadTitle}>{thread.thread_title}</h3>
-              <div className={styles.bar}>
-                <p className={styles.author}>
-                  Posted by: {thread.author}{" "}
-                  <span className={styles.date}>
-                    {formatDate(thread.created_at)}
-                  </span>
-                </p>
+          <div className={styles.threadContent}>
+            <h3 className={styles.threadTitle}>{thread.thread_title}</h3>
+          </div>
+          <div className={styles.bar}>
+            <p className={styles.author}>
+              Posted by: {thread.author}{" "}
+              <span className={styles.date}>
+                {formatDate(thread.created_at)}
+              </span>
+            </p>
+            <div className={styles.buttonGroup}>
+              <div onClick={handleButtonClick}>
+                <button className={styles.button}>Comment</button>
+              </div>
+              <div onClick={handleButtonClick}>
+                <button className={styles.button}>Share</button>
+              </div>
+              <div className={styles.dropdownContainer}>
+                <button
+                  className={`${styles.button} ${styles.dropdownButton}`}
+                  onClick={(event) => toggleDeleteDropdown(event)}
+                >
+                  <FontAwesomeIcon icon={faEllipsisH} />
+                </button>
+                {showDeleteDropdown && (
+                  <div className={styles.dropdownContent}>
+                    <button
+                      className={styles.deleteButton}
+                      onClick={(event) => {
+                        event.stopPropagation();
+                        handleDeleteThread(thread.thread_id);
+                      }}
+                    >
+                      Delete
+                    </button>
+                  </div>
+                )}
               </div>
             </div>
           </div>
-        </Link>
+        </div>
       ))}
     </div>
   );
