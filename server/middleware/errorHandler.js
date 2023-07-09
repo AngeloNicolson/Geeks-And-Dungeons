6471ea782223a4f316003e945a7f829e43f37feb
AngeloNicolson <69103703+AngeloNicolson@users.noreply.github.com>
2023-07-09 15:59:30 +1200

    Updated error handling to pick up unathorized and send the propper status to the client

--- server/middleware/errorHandler.js
+++ server/middleware/errorHandler.js
@@ -3,6 +3,15 @@ const errorHandler = (error, request, response, next) => {
   const statusCode = error.status || 500;
   const errorMessage = error.message || "Internal Server Error";
 
+  // Check for Unauthorized error
+  if (errorMessage === "Unauthorized") {
+    return response.status(401).json({
+      error: {
+        message: "Unauthorized access",
+      },
+    });
+  }
+
   return response.status(statusCode).json({
     error: {
       message: errorMessage,
