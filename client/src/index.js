31eac37c647b48d73a4ecddb7cd1da7b228327ff
AngeloNicolson <69103703+AngeloNicolson@users.noreply.github.com>
2023-07-21 09:46:39 +1200

    Trying to get navigation to geeks and dungeons working

--- client/src/index.js
+++ client/src/index.js
@@ -9,7 +9,7 @@ import "./index.css";
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <BrowserRouter basename="/">
+    <BrowserRouter>
       <Auth0ProviderWithNavigate>
         <App />
       </Auth0ProviderWithNavigate>
