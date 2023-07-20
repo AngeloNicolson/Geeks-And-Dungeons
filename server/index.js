1365c61cc9ac7298f80025daf080f2972069f762
AngeloNicolson <69103703+AngeloNicolson@users.noreply.github.com>
2023-07-21 09:53:45 +1200

    Trying to get navigation to geeks and dungeons working

--- server/index.js
+++ server/index.js
@@ -1,6 +1,10 @@
 const app = require("./app");
+const path = require("path");
 
-const port = 5000;
+const port = process.env.PORT || 5000;
+
+// Serve static files from the React app build folder
+app.use(express.static(path.join(__dirname, "../client/build")));
 
 app.listen(port, () => {
   console.log(`Server is running on ${port}`);
