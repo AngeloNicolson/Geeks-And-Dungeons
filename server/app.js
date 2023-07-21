7c30a2a128c1d7a68129655084c5fe692dda56d5
AngeloNicolson <69103703+AngeloNicolson@users.noreply.github.com>
2023-07-21 16:08:10 +1200

    Trying to get render to connect to the server

--- server/app.js
+++ server/app.js
@@ -44,7 +44,7 @@ app.use(joiErrorHandler);
 app.use(errorHandler);
 
 app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "/client/src/index.html"));
+  res.sendFile(path.join(__dirname, "/client/src/index.js"));
 });
 
 module.exports = app;
